be3b871b12d2f4bbabfa7ad347ec535a38e89517
Sumit Chaskar <121004381+sumitchaskar7000@users.noreply.github.com>
2025-12-06 18:57:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173'],
+  origin: ['https://jadhavarcbse.com/'],
   credentials: true
 }));
 app.use(express.json());
